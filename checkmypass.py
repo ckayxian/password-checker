acb270fe6be5d220f1b4aac9cd9018776adf5876
ckayxian <ckayx15@gmail.com>
2022-08-08 15:38:18 -0700

    improved main function and add sys.exit

--- checkmypass.py
+++ checkmypass.py
@@ -10,6 +10,7 @@ def request_api_data(query_char):
         raise RuntimeError(f"Error fetching: {response.status_code}, check the api and try again")
     return response
 
+
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
@@ -41,4 +42,5 @@ def main(args):
     return 'done!'
 
 
-main(sys.argv[1:])
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
