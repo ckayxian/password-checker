c848c250f00102ca86715a94d6e7bf11b8cc97c0
ckayxian <ckayx15@gmail.com>
2022-08-08 15:01:17 -0700

    Set up the api request and sha1 hash function

---
+++ checkmypass.py
@@ -0,0 +1,19 @@
+import requests
+import hashlib
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(f"Error fetching: {response.status_code}, check the api and try again")
+    return response
+
+
+def pwned_api_check(password):
+    # check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
+
+
+
